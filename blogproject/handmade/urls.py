dc084c60d9a8b77c69ce838c4dec87b0d8379d73
张立享 <846117898@qq.com>
2018-08-15 00:21:50 +0800

    创建手工饰品工程

---
+++ blogproject/handmade/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+from . import views
+
+app_name = 'handmade'
+urlpatterns = [
+    url(r'^handmade/$', views.handmadeView, name='index'),
+
+]
